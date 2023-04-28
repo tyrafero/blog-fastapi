22ced7207565a8d9b0291ce09eafd4a5e4f32f8d
tyrafero <ashutoshsinghkhadka@gmail.com>
2023-04-28 11:24:52 +0545

    feat: get all users and blogs

--- main.py
+++ main.py
@@ -36,14 +36,26 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(getdb)):
     db.refresh(db_user)
     return db_user
 
-# @app.get('/blog/{blog_id}', response_model=schemas.Blog)
-# def show_blog(blog_id: int, db: Session = Depends(getdb)):
-#     db_blog = db.query(Blog).filter(Blog.id == blog_id).first()
-#     if not db_blog:
-#         raise HTTPException(status_code=404, detail='Blog not found')
-#     return db_blog.__dict__
+@app.get('/blog/{blog_id}', response_model=schemas.Blog)
+def show_blog(blog_id: int, db: Session = Depends(getdb)):
+    db_blog = db.query(Blog).filter(Blog.id == blog_id).first()
+    if not db_blog:
+        raise HTTPException(status_code=404, detail='Blog not found')
+    return db_blog
 
-# @app.get('/blog/list', response_model=List[schemas.Blog])
-# def list_blogs(db: Session = Depends(getdb)):
-#     blogs = db.query(Blog).all()
-#     return blogs.__dict__
+@app.get('/user/{user_id}', response_model=schemas.User)
+def show_user(user_id: int, db: Session = Depends(getdb)):
+    db_blog = db.query(User).filter(User.id == user_id).first()
+    if not db_blog:
+        raise HTTPException(status_code=404, detail='Blog not found')
+    return db_blog
+
+@app.get('/blog')
+def all_blogs(db: Session = Depends(getdb)):
+    db_blog = db.query(Blog).all()
+    return db_blog
+
+@app.get('/user')
+def all_users(db: Session = Depends(getdb)):
+    db_blog= db.query(User).all()
+    return db_blog
